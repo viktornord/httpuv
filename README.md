ff855f356242a017ef584dd2b3b7c221c40a6c81
viktornord1 <viktornord1@gmail.com>
2019-12-27 19:35:00 +0200

    publish

---
+++ README.md
@@ -0,0 +1,52 @@
+# httpuv
+
+**httpuv** is a node.js library for making http requests with a clear API and zero dependencies.
+
+- Simple API
+- Lightweight ( **< 3 KB** )
+- Zero dependencies
+
+##API
+
+##### GET
+```
+httpuv.get(url[, { [headers]: { ... }, [query]: { ... } }]);
+```
+##### POST
+```
+httpuv.post(url[, { [headers]: { ... }, [query]: { ... }, body: ... }]);
+```
+##### PUT
+```
+httpuv.put(url[, { [headers]: { ... }, [query]: { ... }, body: ... }]);
+```
+##### PATCH
+```
+httpuv.patch(url[, { [headers]: { ... }, [query]: { ... }, body: ... }]);
+```
+##### DELETE
+```
+httpuv.delete(url[, { [headers]: { ... }, [query]: { ... } }]);
+```
+
+## Usage
+```javascript
+const httpuv = require('httpuv');
+
+...
+// inside async function
+// Getting data with query params
+const response = await httpuv.get('https://foo.com/bar', {
+  query: { baz: 1 }
+}); // results in GET https://foo.com/bar?baz=1
+
+
+...
+// inside async function
+// Posting a data with headers
+const result = await httpuv.post('https://foo.com/bar', {
+  body: { baz: 1 },
+  headers: { 'Authentication': 'Bearer ...' },
+})
+  
+```
---
+++ http-client.js
@@ -0,0 +1,95 @@
+const querystring = require('querystring');
+const URL = require('url');
+const https = require('https');
+
+const RESPONSE_TYPES = {
+  JSON: 'application/json',
+  TEXT: 'text/html',
+};
+
+function request(url, {
+  method = 'GET', query: params, headers = {}, data,
+} = {}) {
+  const strigifiedParams = querystring.stringify(params);
+  return new Promise((resolve, reject) => {
+    const {
+      path, hostname, port, query,
+    } = URL.parse(url);
+    const requestInst = https.request({
+      hostname,
+      port,
+      path,
+      method,
+      headers,
+      query: [query, strigifiedParams].filter(val => val).join('&'),
+    }, (res) => {
+      const responseType = res['content-type'] || RESPONSE_TYPES.JSON;
+      let rawResponse = Buffer.from([]);
+      res
+        .on('data', (newData) => {
+          rawResponse = Buffer.concat([rawResponse, newData]);
+        })
+        .on('error', reject)
+        .on('end', () => {
+          try {
+            resolve(handleResponse({ statusCode: res.statusCode, rawResponse, responseType }));
+          } catch (error) {
+            reject(error);
+          }
+        });
+    }).on('error', reject);
+    if (data) {
+      requestInst.write(typeof data === 'string' ? data : JSON.stringify(data));
+    }
+    requestInst.end();
+  });
+}
+
+function handleResponse({ statusCode, responseType, rawResponse }) {
+  const handleResponseByStatus = (responseData) => {
+    if (statusCode >= 400) {
+      throw responseData;
+    }
+    return responseData;
+  };
+  const rawData = rawResponse.toString();
+  if (responseType.includes(RESPONSE_TYPES.JSON)) {
+    let response;
+    try {
+      response = rawData && JSON.parse(rawData);
+    } catch (e) {
+      throw statusCode >= 400 ? rawData : e;
+    }
+    return handleResponseByStatus(response);
+  }
+  if (responseType.includes(RESPONSE_TYPES.TEXT)) {
+    return handleResponseByStatus(rawData);
+  }
+  return handleResponseByStatus(rawResponse);
+}
+
+const httpClient = {
+  get(url, { headers, query } = {}) {
+    return request(url, { headers, query });
+  },
+  post(url, { headers, query, body: data } = {}) {
+    return request(url, {
+      method: 'POST', headers, query, data,
+    });
+  },
+  put(url, { headers, query, body: data } = {}) {
+    return request(url, {
+      method: 'PUT', headers, query, data,
+    });
+  },
+  patch(url, { headers, query, body: data } = {}) {
+    return request(url, {
+      method: 'PATCH', headers, query, data,
+    });
+  },
+  delete(url, { headers, query } = {}) {
+    return request(url, { method: 'DELETE', headers, query });
+  },
+};
+
+module.exports.httpClient = httpClient;
